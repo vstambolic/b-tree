a0a1385994f7d4bc7826df15bc1e3f34f7a6d70e
Vasilije Stambolić <61201104+vstambolic@users.noreply.github.com>
2020-03-15 04:15:41 +0100

    Add files via upload

---
+++ T_Node.cpp
@@ -0,0 +1,22 @@
+#include "T_Node.h"
+
+T_Node * T_Node::leftmost(T_Node* ptr) const {
+	T_Node* tmp = ptr;
+	while (!tmp->is_leaf())
+		tmp = tmp->P[0];
+
+	return tmp;
+}
+
+T_Node * T_Node::successor(int pos) const {
+	if (is_leaf())
+		return nullptr;
+	else
+		return leftmost(P[pos + 1]);
+
+}
+
+
+
+
+
